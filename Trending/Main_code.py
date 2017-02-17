013330b9071812554a5b4fe9eec8d5319dac661d
dushyantRathore <dushyant.bgs@gmail.com>
2017-02-17 13:22:57 +0530

    Updated

--- Trending/Main_code.py
+++ Trending/Main_code.py
@@ -4,23 +4,15 @@ import Trending_dev as td
 import Github_user as gu
 
 @click.command()
-@click.option('--repo', '-r', default="today", help="Lists the trending repositories.")
+@click.option('--repo', '-r', is_flag=True, help="Lists the trending repositories.")
 @click.option('--dev', '-d', is_flag=True, help="Lists the trending developers.")
 @click.option('--username', '-user', default="dushyantRathore", help='Lists the details of a user on Github')
 
 
 def cli(repo,dev,username):
-
     if repo:
-        if repo == "today":
-            url = "https://github.com/trending?since=daily"
-            tr.getRepo(url)
-        elif repo == "week":
-            url = "https://github.com/trending?since=weekly"
-            tr.getRepo(url)
-        elif repo =="month":
-            url = "https://github.com/trending?since=monthly"
-            tr.getRepo(url)
+        url = "https://github.com/trending?since=daily"
+        tr.getRepo(url)
 
     elif dev:
         url = "https://github.com/trending/developers?since=daily"
